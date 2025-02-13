e98f232944d9e31c859470f77e4564603f68ddd9
JunaidP24 <junaidpop12@gmail.com>
2025-02-12 22:53:17 -0800

    debugging...

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,4 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
-console.log('API URL:', process.env.REACT_APP_API_URL);
-console.log('Final API URL:', API_URL);
+const API_URL = 'https://takehome-uhoe.onrender.com';
+console.log('Using API URL:', API_URL);
 export default API_URL; 
