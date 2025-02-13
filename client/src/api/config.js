faa3205e789095284d8c7240ba4995ec9d88f6b8
JunaidP24 <junaidpop12@gmail.com>
2025-02-12 22:48:48 -0800

    debugging

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,2 +1,4 @@
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+console.log('API URL:', process.env.REACT_APP_API_URL);
+console.log('Final API URL:', API_URL);
 export default API_URL; 
